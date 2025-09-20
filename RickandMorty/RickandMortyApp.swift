20ce3c7a1a90a57cdc6781924790bbb6f7363221
Rafael GPL <rafael.gpl@dashws.com>
2025-09-20 11:54:12 -0700

    Initial Commit

---
+++ RickandMorty/RickandMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyApp.swift
+//  RickandMorty
+//
+//  Created by Rafael GPL on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
